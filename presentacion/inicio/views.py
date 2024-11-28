005a42bb6e9f93db0f8f3231c381ae75073625f2
Muners24 <ramon.mejia@uabc.edu.mx>
2024-11-28 02:37:10 -0800

    -key

--- presentacion/inicio/views.py
+++ presentacion/inicio/views.py
@@ -8,17 +8,16 @@ import base64
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
-from negocios.pefiles.Perfil import Perfil
+from negocios.pefiles.N_Perfil import N_Perfil
 
 def inicio(request):
     
     if request.session.get('IdUsuario') == None:
         return redirect('/logout')
 
-    NP = Perfil()
+    NP = N_Perfil()
     
     perfil = NP.buscarPerfilPorIdUsuario(request.session.get('IdUsuario'))
-    
    
     return render(request, "Inicio.html",{'img': perfil.getImg()})
 
